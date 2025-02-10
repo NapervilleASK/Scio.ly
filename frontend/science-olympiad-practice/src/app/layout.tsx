53fa85592111193bc4158b40923e7405aa60de10
Kudos <kudosbeluga@gmail.com>
2025-02-09 20:59:54 -0600

    idk

--- frontend/science-olympiad-practice/src/app/layout.tsx
+++ frontend/science-olympiad-practice/src/app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Scio.ly",
   description: "The Ultimate Study Guide",
+  icons: {
+    icon: "./favicon.ico"
+  }
 };
 
 export default function RootLayout({
@@ -26,6 +29,9 @@ export default function RootLayout({
   return (
     <Suspense>
     <html lang="en">
+      <head>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
