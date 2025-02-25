5b29081e3d3193ded5818c9ca84322560296aa4e
Kudos <kudosbeluga@gmail.com>
2025-02-25 17:38:30 -0600

    wompwomp

--- frontend/science-olympiad-practice/src/app/api.tsx
+++ frontend/science-olympiad-practice/src/app/api.tsx
@@ -1,4 +1,4 @@
 const api = {
-    api: 'https://gist.githubusercontent.com/Kudostoy0u/3cfeca510d776957fbad449fcdd6f402/raw/f3f4ee62c5556c5898dc11e3485aefc1552ab873/final.json'
+    api: 'https://gist.githubusercontent.com/Kudostoy0u/404ac5c27405f274b350469f29573f61/raw/d63153f9ce19f5626133e38b885dcc0699a32065/final.json'
 };
 export default api  
